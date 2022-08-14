cb64bfd3233b89db4612c60690041ba0864d79a9
Walter <WalterInKitchen@outlook.com>
2022-08-14 18:50:01 +0800

    get base url from window

--- docs/.vuepress/theme/assets/urls.js
+++ docs/.vuepress/theme/assets/urls.js
@@ -1,11 +1,11 @@
-const base = "https://walterInKitchen.github.io";
+const base = window.location.origin;
 
 const urls = {
-    homeAddress: base,
+    homeAddress: base + '/',
     timelineAddress: base + "/timeline",
     githubHome: "https://github.com/WalterInKitchen",
     emailAddress: "walterInKitchen@outlook.com",
     telegramUserName: "walterinkitchen",
 };
 
-export { urls };
+export {urls};
