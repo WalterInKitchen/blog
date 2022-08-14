3694175fbd2ce717d184d64bb4ed08eae9383108
Walter <WalterInKitchen@outlook.com>
2022-08-14 18:54:58 +0800

    get baseurl in runtime

--- docs/.vuepress/theme/assets/urls.js
+++ docs/.vuepress/theme/assets/urls.js
@@ -1,11 +1,12 @@
-const base = window.location.origin;
-
 const urls = {
-    homeAddress: base + '/',
-    timelineAddress: base + "/timeline",
     githubHome: "https://github.com/WalterInKitchen",
     emailAddress: "walterInKitchen@outlook.com",
     telegramUserName: "walterinkitchen",
 };
 
-export {urls};
+const homeAddress = () => {
+    const base = window.location.origin;
+    return base + '/';
+}
+
+export {urls, homeAddress};
